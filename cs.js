ae5ef14d3113d4d7659658f2d046ce4796a5d55f
Jerin <taurenshaman@outlook.com>
2019-08-30 13:39:52 +0800

    add image support

--- cs.js
+++ cs.js
@@ -1,43 +1,80 @@
 /**
  * Browser console hack that allows you to save JSON objects to disk.
- *
+ * @project https://github.com/wellsjo/console.save
  * @author Wells Johnston
  */
 
-(function(console) {
+(function (console) {
 
-  /**
-   * Save json object to disk.
+    /**
+     * Save json object to disk.
+     *
+     * @param {object} obj The object to save.
+     * @param {string} filename The name of the file to save.
+     */
+    console.saveJson = function (obj, filename) {
+
+        if (!obj) {
+            console.error('console.saveJson: No data.');
+            return;
+        }
+
+        if (filename === null) filename = 'console.json';
+
+        if (typeof obj === 'object') {
+            var data = JSON.stringify(obj, undefined, 4);
+        }
+
+        var blobType = 'text/json';
+        // create a phantom link element to download a Blob of data, then create
+        // a mouse event to click the link to download the file.
+        var blob = new Blob([data], {
+            type: blobType
+        });
+        var e = document.createEvent('MouseEvents');
+        var a = document.createElement('a');
+
+        a.download = filename;
+        a.href = window.URL.createObjectURL(blob);
+        a.dataset.downloadurl = [blobType, a.download, a.href].join(':');
+        e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+        a.dispatchEvent(e);
+    };
+
+    /**
+   * Save image object to disk.
    *
    * @param {object} obj The object to save.
    * @param {string} filename The name of the file to save.
+   * @param {string} imageType The type of the image to save: png, jpeg, gif
    */
-  console.save = function(obj, filename) {
+    console.saveImage = function (obj, filename, imageType) {
 
-    if (!obj) {
-      console.error('console.save: No data.');
-      return;
-    }
+        if (!obj) {
+            console.error('console.saveImage: No data.');
+            return;
+        }
 
-    if (filename == null) filename = 'console.json';
+        if (filename === null) filename = 'console.jpeg';
 
-    if (typeof obj === 'object') {
-      var data = JSON.stringify(obj, undefined, 4);
-    }
+        //if (typeof obj === 'object') {
+        //    var data = JSON.stringify(obj, undefined, 4);
+        //}
 
-    // create a phantom link element to download a Blob of data, then create
-    // a mouse event to click the link to download the file.
-    var blob = new Blob([data], {
-      type: 'text/json'
-    });
-    var e = document.createEvent('MouseEvents');
-    var a = document.createElement('a');
+        var blobType = 'image/' + imageType;
+        // create a phantom link element to download a Blob of data, then create
+        // a mouse event to click the link to download the file.
+        var blob = new Blob([obj], {
+            type: blobType
+        });
+        var e = document.createEvent('MouseEvents');
+        var a = document.createElement('a');
 
-    a.download = filename;
-    a.href = window.URL.createObjectURL(blob);
-    a.dataset.downloadurl = ['text/json', a.download, a.href].join(':');
-    e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-    a.dispatchEvent(e);
-  };
+        a.download = filename;
+        a.href = window.URL.createObjectURL(blob);
+        a.dataset.downloadurl = [blobType, a.download, a.href].join(':');
+        e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+        a.dispatchEvent(e);
+    };
 
 })(console);
